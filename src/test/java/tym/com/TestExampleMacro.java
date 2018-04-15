a2566842750d868bc3943c3e0d0f9cbee140d986
ronnick <ronnick@163.com>
2018-04-15 21:05:31 +0800

    edit .gitignore

---
+++ src/test/java/tym/com/TestExampleMacro.java
@@ -0,0 +1,14 @@
+package tym.com;
+
+import junit.framework.TestCase;
+
+/**
+ * Testing {@link tym.com.ExampleMacro}
+ */
+public class TestExampleMacro extends TestCase
+{
+    public void testBasic()
+    {
+        // add test here...
+    }
+}
