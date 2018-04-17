fc233772da4dc0236aa6564a6aa11f6940edfff7
ronnick <ronnick@163.com>
2018-04-18 01:34:46 +0800

    test object.

--- trunk/hello-world/src/main/java/com/tym/helloworld/HelloWorld.java
+++ trunk/hello-world/src/main/java/com/tym/helloworld/HelloWorld.java
@@ -3,5 +3,8 @@ package com.tym.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Object object = new Object();
+        System.out.println(object.getClass());
     }
 }
